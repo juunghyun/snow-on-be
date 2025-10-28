84e2c2638e00f679d76142411abd3728ff61762c
juunghyun <forcomfe81@gmail.com>
2025-10-28 16:26:59 +0900

    Initial: project setup

---
+++ src/main/java/com/snowon/snowon/SnowonApplication.java
@@ -0,0 +1,13 @@
+package com.snowon.snowon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnowonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snowon/snowon/SnowonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snowon.snowon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
